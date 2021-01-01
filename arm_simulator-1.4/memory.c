9e70863ffffdd28520513d3b9ec3b8d300e13448
Esdia <theo.hermitte@protonmail.com>
2021-01-01 16:21:22 +0100

    Implementation de la memoire

--- arm_simulator-1.4/memory.c
+++ arm_simulator-1.4/memory.c
@@ -1,64 +1,147 @@
 /*
-Armator - simulateur de jeu d'instruction ARMv5T à but pédagogique
+Armator - simulateur de jeu d'instruction ARMv5T ï¿½ but pï¿½dagogique
 Copyright (C) 2011 Guillaume Huard
 Ce programme est libre, vous pouvez le redistribuer et/ou le modifier selon les
-termes de la Licence Publique Générale GNU publiée par la Free Software
-Foundation (version 2 ou bien toute autre version ultérieure choisie par vous).
+termes de la Licence Publique Gï¿½nï¿½rale GNU publiï¿½e par la Free Software
+Foundation (version 2 ou bien toute autre version ultï¿½rieure choisie par vous).
 
-Ce programme est distribué car potentiellement utile, mais SANS AUCUNE
+Ce programme est distribuï¿½ car potentiellement utile, mais SANS AUCUNE
 GARANTIE, ni explicite ni implicite, y compris les garanties de
-commercialisation ou d'adaptation dans un but spécifique. Reportez-vous à la
-Licence Publique Générale GNU pour plus de détails.
+commercialisation ou d'adaptation dans un but spï¿½cifique. Reportez-vous ï¿½ la
+Licence Publique Gï¿½nï¿½rale GNU pour plus de dï¿½tails.
 
-Vous devez avoir reçu une copie de la Licence Publique Générale GNU en même
-temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software
+Vous devez avoir reï¿½u une copie de la Licence Publique Gï¿½nï¿½rale GNU en mï¿½me
+temps que ce programme ; si ce n'est pas le cas, ï¿½crivez ï¿½ la Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
-États-Unis.
+ï¿½tats-Unis.
 
 Contact: Guillaume.Huard@imag.fr
-	 Bâtiment IMAG
+	 Bï¿½timent IMAG
 	 700 avenue centrale, domaine universitaire
-	 38401 Saint Martin d'Hères
+	 38401 Saint Martin d'Hï¿½res
 */
 #include <stdlib.h>
 #include "memory.h"
 #include "util.h"
 
+/*
+  La mÃ©moire sera reprÃ©sentÃ©e comme ceci:
+    - Un tableau d'entiers
+      - Chaque case du tableau correspond Ã  un octet.
+      - L'adresse d'un octet dans la mÃ©moire est l'indice de sa case dans le
+        tableau
+      - La taille de la mÃ©moire est le nombre d'octets qui la compose, donc
+        la taille du tableau
+    - Un entier qui correspond Ã  la taille de la mÃ©moire
+    - Un entier qui indique si la mÃ©moire est en big ou little endian
+*/
 struct memory_data {
+    uint8_t* data;
+    size_t size;
+    int is_big_endian;
 };
 
 memory memory_create(size_t size, int is_big_endian) {
-    memory mem=NULL;
+    memory mem = malloc(sizeof(struct memory_data));
+    
+    mem->size = size;
+    mem->data = malloc(size * sizeof(uint8_t));
+    
+    mem->is_big_endian = is_big_endian;
     return mem;
 }
 
 size_t memory_get_size(memory mem) {
-    return 0;
+    return mem->size;
 }
 
 void memory_destroy(memory mem) {
+    free(mem->data);
+    free(mem);
 }
 
 int memory_read_byte(memory mem, uint32_t address, uint8_t *value) {
-    return -1;
+    if(address >= memory_get_size(mem)) return 0;
+
+    *value = mem->data[address];
+    return 1;
 }
 
 int memory_read_half(memory mem, uint32_t address, uint16_t *value) {
-    return -1;
+    // On fait -1 car on lit l'octet Ã  address, et celui d'aprÃ¨s
+    if(address >= memory_get_size(mem) - 1) return 0;
+
+    // Un half-word s'Ã©crit sur une adresse multiple de 2
+    if(address % 2 != 0) return 0;
+
+    if(mem->is_big_endian) {
+        *value = ((uint16_t) mem->data[address]) << 8 | mem->data[address+1];
+    } else {
+        *value = ((uint16_t) mem->data[address+1]) << 8 | mem->data[address];
+    }
+    return 1;
 }
 
 int memory_read_word(memory mem, uint32_t address, uint32_t *value) {
-    return -1;
+    // On fait -3 car on lit l'octet Ã  address, et les trois suivants
+    if(address >= memory_get_size(mem) - 3) return 0;
+
+    // Un word s'Ã©crit sur une adresse multiple de 4
+    if(address % 4 != 0) return 0;
+
+    if(mem->is_big_endian) {
+        *value = (mem->data[address] << 24) | (mem->data[address+1] << 16) | (mem->data[address+2] << 8) | (mem->data[address+3]);
+    } else {
+        *value = (mem->data[address+3] << 24) | (mem->data[address+2] << 16) | (mem->data[address+1] << 8) | (mem->data[address]);
+    }
+    return 1;
 }
 
 int memory_write_byte(memory mem, uint32_t address, uint8_t value) {
-    return -1;
+    if(address >= memory_get_size(mem)) return 0;
+
+    mem->data[address] = value;
+    return 1;
 }
 
 int memory_write_half(memory mem, uint32_t address, uint16_t value) {
-    return -1;
+    // On fait -1 car on Ã©crit sur l'octet Ã  address, et celui d'aprÃ¨s
+    if(address >= memory_get_size(mem) - 1) return 0;
+
+    // Un half-word s'Ã©crit sur une adresse multiple de 2
+    if(address % 2 != 0) return 0;
+
+    uint16_t mask = 0x00FF;
+
+    if(mem->is_big_endian) {
+        mem->data[address] = value >> 8;
+        mem->data[address+1] = value & mask;
+    } else {
+        mem->data[address+1] = value >> 8;
+        mem->data[address] = value & mask;
+    }
+    return 1;
 }
 
 int memory_write_word(memory mem, uint32_t address, uint32_t value) {
-    return -1;
+    // On fait -3 car on lit l'octet Ã  address, et les trois suivants
+    if(address >= memory_get_size(mem) - 3) return 0;
+
+    // Un word s'Ã©crit sur une adresse multiple de 4
+    if(address % 4 != 0) return 0;
+
+    uint32_t mask = 0x000000FF;
+
+    if(mem->is_big_endian) {
+        mem->data[address] = value >> 24;
+        mem->data[address+1] = value >> 16 & mask;
+        mem->data[address+2] = value >> 8 & mask;
+        mem->data[address+3] = value & mask;
+    } else {
+        mem->data[address+3] = value >> 24;
+        mem->data[address+2] = value >> 16 & mask;
+        mem->data[address+1] = value >> 8 & mask;
+        mem->data[address] = value & mask;
+    }
+    return 1;
 }
