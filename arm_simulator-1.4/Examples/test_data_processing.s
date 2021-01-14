31a64482981d9e48f6f8cd0fbd641bd49f63d550
Esdia <theo.hermitte@protonmail.com>
2021-01-14 03:34:30 +0100

    Réécriture du test pour data_processing

--- arm_simulator-1.4/Examples/test_data_processing.s
+++ arm_simulator-1.4/Examples/test_data_processing.s
@@ -1,59 +1,34 @@
 .global main
 .text
 main:
+    @ Test du mov + barrel shifter
     mov r1, #5
     mov r3, #7
     mov r2, r3
-    mov r3, r2, lsl r1
-    mov r3, r2, lsl #1
+    mov r3, r2, lsl r1  @ Attendu : 0xE0
+    mov r3, r2, lsl #1  @ Attendu : 0xE
+    mov r3, r2, lsr #1  @ Attendu : 0x3
+    mov r4, #0x3
+    mov r3, r4, ror #1  @ Attendu : 0x80000001
+    mov r4, #0xF0000000
+    mov r3, r4, asr #1 @ Attendu : 0xF8000000
 
-    and r0, r1, r2
-    eor r0, r1, r2
-    sub r0, r1, r2
-    rsb r0, r1, r2
-    add r0, r1, r2
-    adc r0, r1, r2
-    sbc r0, r1, r2
-    rsc r0, r1, r2
-    tst r1, r2
-    teq r1, r2
-    cmp r1, r2
-    cmn r1, r2
-    orr r0, r1, r2
-    bic r0, r1, r2
-    mvn r0, r1
-
-    mrs r1, cpsr_all
-    msr cpsr_flg, r1
-
-    mov r1, #5
-
-    mul r0, r1, r2
-    mla r0, r1, r2, r3
-
-    umull r5, r6, r1, r2
-    umlal r5, r6, r1, r2
-    smull r5, r6, r1, r2
-    smlal r5, r6, r1, r2
-
-    ldr r1, [r2]
-    str r1, [r2]
-    ldrb r1, [r2]
-    strb r1, [r2]
-    ldrh r1, [r2]
-    ldrsh r1, [r2]
-    ldrsb r1, [r2]
-    strh r1, [r2]
-    
-    ldmfd sp!, {r0, r1, r2}
-    stmia r0, {r0-r15}
-
-    swp r0, r1, [r2]
-    swpb r0, r1, [r2]
-
-    bx lr
-    b end
-    bl end
+    @ r1 <- 5, r2 <- 7
+    and r0, r1, r2  @ 0x5
+    eor r0, r1, r2  @ 0x2
+    sub r0, r1, r2  @ 0xFFFFFFFE
+    rsb r0, r1, r2  @ 0x2
+    add r0, r1, r2  @ 0xC
+    adc r0, r1, r2  @ 0xC
+    sbc r0, r1, r2  @ 0xFFFFFFFD
+    rsc r0, r1, r2  @ 0x1
+    tst r1, r2 @ Poids fort du CPSR : 0x0
+    teq r1, r2 @ Poids fort du CPSR : 0x0
+    cmp r1, r2 @ Poids fort du CPSR : 0x8
+    cmn r1, r2 @ Poids fort du CPSR : 0x0
+    orr r0, r1, r2 @ 0x7
+    bic r0, r1, r2 @ 0x0
+    mvn r0, r1 @ 0xFFFFFFFA
 
 end:
     swi 0x123456
